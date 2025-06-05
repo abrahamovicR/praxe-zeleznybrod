3b044c157603de1ad499f720f3e7dcfaa89a793a
abrahamovicR <richard.abraham.023@pslib.cz>
2025-06-05 10:54:13 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
-const gulp = require('gulp');
+
+
+
+const { src, dest, watch, series } = require('gulp');
 const nunjucksRender = require('gulp-nunjucks-render');
 const data = require('gulp-data');
 const plumber = require('gulp-plumber');
 const fs = require('fs');
 const path = require('path');
+const colors = require('ansi-colors');
+const fancyLog = require('fancy-log');
+const htmlLint = require('gulp-htmllint');
+const prettyError = require('gulp-prettyerror');
 
 // Cesty ke šablonám a datům
 const paths = {
@@ -19,19 +26,57 @@ function getData() {
 
 // Render task
 function render() {
-  return gulp.src(['src/templates/*.html', 'src/templates/**/*.html', '!src/templates/layout.html'])
+  return src(['src/templates/**/*.html', '!src/templates/layout.html'])
+
     .pipe(plumber())
     .pipe(data(getData))
     .pipe(nunjucksRender({
       path: ['src/'] // pro include/extend
     }))
-    .pipe(gulp.dest(paths.dest));
+    .pipe(dest(paths.dest));
 }
 
 // Watcher
 function watchFiles() {
-  gulp.watch(paths.templates, render);
-  gulp.watch(paths.dataFile, render);
+  watch(paths.templates, render);
+  watch(paths.dataFile, render);
 }
 
-exports.default = gulp.series(render, watchFiles);
+function htmlLintTask() {
+  let errorFree = true;
+
+  return src(['src/templates/**/*.html'])
+    .pipe(
+      htmlLint({}, function (filepath, issues) {
+        if (issues.length > 0) {
+          errorFree = false;
+          fancyLog(colors.cyan('[gulp-htmllint]') + ' Error in ' + colors.magenta(filepath));
+        }
+
+        issues.forEach(function (issue) {
+          process.stdout.write(
+            colors.white('line ' + issue.line + ', col ' + issue.column) +
+            '\t\t' +
+            colors.red(issue.msg) +
+            ' ' +
+            colors.white('[' + issue.rule + ':' + issue.code + ']') +
+            '\n'
+          );
+        });
+
+        if (issues.length > 0) {
+          process.exitCode = 1;
+        }
+      })
+    )
+    .on('finish', function (error) {
+      if (errorFree) {
+        fancyLog(colors.green('Task completed successfully!') + ' [_htmlLint]');
+      } else {
+        fancyLog(colors.red('Task failed!'));
+      }
+    });
+}
+
+
+exports.default = series(render, htmlLintTask, watchFiles);
